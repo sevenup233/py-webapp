8c7fb248d381e8c1d184484666f1f8afb0b4aa40
sevenup233 <1149580368@qq.com>
2017-08-10 12:35:39 +0800

    update

--- www/app.py
+++ www/app.py
@@ -109,7 +109,7 @@ async def init(loop):
     add_routes(app, 'handlers')
     add_static(app)
     srv = await loop.create_server(app.make_handler(), '127.0.0.1', 8999)
-    logging.info('server started at http://127.0.0.1:9000...')
+    logging.info('server started at http://127.0.0.1:8999...')
     return srv
 
 loop = asyncio.get_event_loop()
