d707e5a2a5d2178bb2620cbdc75ed9250bc851c1
sevenup233 <1149580368@qq.com>
2017-08-10 23:08:33 +0800

    Day 06

---
+++ www/config.py
@@ -0,0 +1,50 @@
+#合并配置文件
+
+import config_default
+
+#定义一个新的dict类
+
+class Dict(dict):
+    def __init__(self, names=(), values=(), **kw):
+        super(Dict, self).__init__(**kw)
+        for k, v in zip(names, values):
+            self[k] = v
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+#merge函数，合并两个配置文件，返回新配置文件r
+
+def merge(defaults, override):
+    r = {}
+    for k, v in defaults.items():
+        if k in override:
+            if isinstance(v, dict):
+                r[k] = merge(v, override[k])
+            else:
+                r[k] = override[k]
+        else:
+            r[k] = v
+    return r
+
+def toDict(d):
+    D = Dict()
+    for k, v in d.items():
+        D[k] = toDict(v) if isinstance(v, dict) else v
+    return D
+
+configs = config_default.configs
+
+try:
+    import config_override
+    configs = merge(configs, config_override.configs)
+except ImportError:
+    pass
+
+configs = toDict(configs)
---
+++ www/config_default.py
@@ -0,0 +1,14 @@
+#默认配置
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www-data',
+        'password': 'www-data',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
