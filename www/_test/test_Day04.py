01e2472f8015adbc19c42bc2acad88029af65a71
sevenup233 <1149580368@qq.com>
2017-08-12 00:15:44 +0800

    Day 08
    注意，新建html编码要改为utf-8，不然会有大事发生
    干了一天前端，仿佛回到六个月前熬夜做网站。这次可真的是纯手打的个人网站，虽然简陋了点但是看着挺舒服的

--- www/_test/test_Day04.py
+++ www/_test/test_Day04.py
@@ -4,7 +4,7 @@ from models import User, Blog, Comment
 async def test(loop):
 
     await orm.create_pool(loop,user='www-data', password='www-data', db='awesome')
-    u = User(name='hzp', email='1149580369@qq.com', passwd='abcabc789', image='about:blank')
+    u = User(name='root', email='root@qq.com', passwd='root', image='about:blank')
     await u.save()
 
 loop = asyncio.get_event_loop()
